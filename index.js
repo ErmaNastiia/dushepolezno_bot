c8babf9ceeefed0cd864d0463e1111f5e7965821
ErmaNastiia <epmabonne@gmail.com>
2025-03-26 23:12:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ const token = process.env.REFRESH_TOKEN;
 const user = process.env.EMAIL_USER;
 const pass = process.env.EMAIL_PASSWORD;
 
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
 oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 const calendar = google.calendar({
   version: 'v3',
