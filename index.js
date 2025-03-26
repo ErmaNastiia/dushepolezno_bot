41cd719d32d8afa5b3151196e0478c1569e31a27
ErmaNastiia <epmabonne@gmail.com>
2025-03-27 00:36:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const { OAuth2 } = google.auth;
 const { freeStorage } = require('@grammyjs/storage-free');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 // Initialize the bot
 const bot = new Bot(process.env.BOT_API_KEY);
 
