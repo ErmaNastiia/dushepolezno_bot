6912e842939ea18b15ed8325fe8c27e144892bea
ErmaNastiia <epmabonne@gmail.com>
2025-03-26 19:50:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const { OAuth2 } = google.auth;
 const { freeStorage } = require('@grammyjs/storage-free');
 // Initialize the bot
 const bot = new Bot(process.env.BOT_API_KEY);
-const punycode = require('punycode/');
+
+const port = process.env.PORT || 5000
+
 const oAuth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
